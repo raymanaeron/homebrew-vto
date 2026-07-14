65ccecb8d5a6fb7b31530eae38e649736badef82
Elysian Edge <elysian.edge.nv@gmail.com>
2026-07-13 23:12:58 -0700

    vto 0.1.0

---
+++ Formula/vto.rb
@@ -0,0 +1,22 @@
+class Vto < Formula
+  desc "viato.me tunnel client — expose localhost to the internet"
+  homepage "https://viato.me"
+  version "0.1.0"
+  license "MIT"
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://viato.me/api/v1/releases/darwin-arm64"
+    sha256 "de4f880929232ea0dcaf88d505944c27738cc4afaed61162b7f1cdaea3bae5d0"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://viato.me/api/v1/releases/linux-amd64"
+    sha256 "b85b5a9ea37dc43b8b74d06672dec4d4e11ed314f08781c7ff242d3199731dce"
+  end
+
+  def install
+    bin.install "vto"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("\#{bin}/vto --version")
+  end
+end
